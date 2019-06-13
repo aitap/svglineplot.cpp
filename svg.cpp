05326a0c7a3a37b6927f665cf4d2f048926844f7
Ivan Krylov <krylov.r00t@gmail.com>
2019-06-13 18:15:50 +0300

    drop polylines - cannot use relative coordinates

--- svg.cpp
+++ svg.cpp
@@ -9,21 +9,27 @@ struct svgplot {
 		std::size_t n;
 	};
 
-	unsigned int width, height;
+	unsigned int width, height; // em
 	struct {
 		// min, max
-		double x[2], y[2];
+		double x[2], y[2]; // plot coordinates
 	} axes;
+
+	enum mar { bottom, left, top, right, mar_max };
+	unsigned int margins[mar_max]; // em
 
 	std::vector<data> lines;
 
-	svgplot() : width(800), height(600) {
+	svgplot() : width(30), height(20) {
 		// limits start with "impossible" values that would
 		// lose to real data in std::min() / std::max() comparison
 		axes.x[0] = std::numeric_limits<double>::max();
 		axes.x[1] = std::numeric_limits<double>::min();
 		axes.y[0] = std::numeric_limits<double>::max();
 		axes.y[1] = std::numeric_limits<double>::min();
+
+		margins[bottom] = margins[left] = 3;
+		margins[top] = margins[right] = 1;
 	}
 
 	svgplot & add_line(const double * x, const double * y, std::size_t n) {
@@ -43,27 +49,35 @@ struct svgplot {
 		std::stringstream ss;
 		ss.imbue(std::locale("C")); // prevent locale-related float formatting problems
 
-		ss << "<?xml version=\"1.0\" standalone=\"no\"?>\n"
-			<< "<svg"
-			<< " width=\"" << width << "\" height=\"" << height << "\""
-			<< " version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\""
-			<< ">\n"
-			<< "<g style=\"fill: none; stroke: black; stroke-width: 1px;\">\n";
+		ss << "<svg"
+			" width=\"" << width << "em\" height=\"" << height << "em\""
+			" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\""
+			">\n"
+			"<g style=\"fill: none; stroke: black; stroke-width: 1px;\">\n"
+			"<svg" // plot area starts
+			" x=\"" << margins[left] << "em\" y=\"" << margins[top] << "em\""
+			" width=\"" << width - margins[right] - margins[left] << "em\""
+			" height=\"" << height - margins[bottom] - margins[top] << "em\""
+			">\n";
 
-		// TODO: axes and stuff
-		ss << "<rect"
-			<< " width=\"100%\" height=\"100%\""
-			<< "/>\n";
+		// axes
+		ss << "<line x1=\"0%\" y1=\"100%\" x2=\"0%\" y2=\"0%\"/>"
+			"<line x1=\"0%\" y1=\"100%\" x2=\"100%\" y2=\"100%\"/>";
 
 		for (size_t i = 0; i < lines.size(); i++) {
-			ss << "<path d=\"";
-			for (size_t j = 0; j < lines[i].n; j++)
-				ss << (j ? " L" : "M")
-					<< width * (lines[i].x[j] - axes.x[0])/(axes.x[1] - axes.x[0])
-					<< ','
-					<< height * (axes.y[1] - lines[i].y[j])/(axes.y[1] - axes.y[0]);
-			ss << "\"/>\n";
+			for (size_t j = 1; j < lines[i].n; j++)
+				// screw this, polylines and paths are unsuitable for relative or physical positions
+				// let's do it the hard way, then
+				ss << "<line"
+					" x1=\"" << (lines[i].x[j-1] - axes.x[0])/(axes.x[1] - axes.x[0]) * 100 << "%\""
+					" y1=\"" << (axes.y[1] - lines[i].y[j-1])/(axes.y[1] - axes.y[0]) * 100 << "%\""
+					" x2=\"" << (lines[i].x[j] - axes.x[0])/(axes.x[1] - axes.x[0]) * 100 << "%\""
+					" y2=\"" << (axes.y[1] - lines[i].y[j])/(axes.y[1] - axes.y[0]) * 100 << "%\""
+					"/>";
+			ss << "\n";
 		}
+
+		ss << "</svg>\n"; // plot area ends, can draw labels now
 
 		ss << "</g></svg>\n";
 
