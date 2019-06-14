d3a0a359a01fcb03f5a0c97f47d6eb49be8b72c0
Ivan Krylov <krylov.r00t@gmail.com>
2019-06-14 20:06:44 +0300

    use SVG Tiny 1.2 non-scaling stroke + paths
    - this gives us a fairly effective representation, i.e. the whole plot
      area is a single <svg viewBox=""><path ... /></svg>
    - to make the plot "responsive", we also ditch <svg> width & height
     - which means farewell to margins in physical units and welcome %
       instead
      - which probably means that I need to use vw and vh for label font
        sizes
    - axes are still TODO
     - see "Nice Numbers for Graph Labels" by Paul Heckbert for the
       algorithm [doi:10.1016/b978-0-08-050753-8.50025-5]

--- svg.cpp
+++ svg.cpp
@@ -9,18 +9,18 @@ struct svgplot {
 		std::size_t n;
 	};
 
-	unsigned int width, height; // em
+	unsigned int ticks; // how many ticks to place on the axes, TODO
+
 	struct {
-		// min, max
-		double x[2], y[2]; // plot coordinates
+		double x[2], y[2]; // plot coordinates: {min, max}
 	} axes;
 
 	enum mar { bottom, left, top, right, mar_max };
-	unsigned int margins[mar_max]; // em
+	unsigned int margins[mar_max]; // %
 
 	std::vector<data> lines;
 
-	svgplot() : width(30), height(20) {
+	svgplot() : ticks(4) {
 		// limits start with "impossible" values that would
 		// lose to real data in std::min() / std::max() comparison
 		axes.x[0] = std::numeric_limits<double>::max();
@@ -28,8 +28,8 @@ struct svgplot {
 		axes.y[0] = std::numeric_limits<double>::max();
 		axes.y[1] = std::numeric_limits<double>::min();
 
-		margins[bottom] = margins[left] = 3;
-		margins[top] = margins[right] = 1;
+		margins[bottom] = margins[left] = 10;
+		margins[top] = margins[right] = 5;
 	}
 
 	svgplot & add_line(const double * x, const double * y, std::size_t n) {
@@ -50,34 +50,34 @@ struct svgplot {
 		ss.imbue(std::locale("C")); // prevent locale-related float formatting problems
 
 		ss << "<svg"
-			" width=\"" << width << "em\" height=\"" << height << "em\""
-			" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\""
+			" version=\"1.2\" baseProfile=\"tiny\" xmlns=\"http://www.w3.org/2000/svg\""
 			">\n"
 			"<g style=\"fill: none; stroke: black; stroke-width: 1px;\">\n"
-			"<svg" // plot area starts
-			" x=\"" << margins[left] << "em\" y=\"" << margins[top] << "em\""
-			" width=\"" << width - margins[right] - margins[left] << "em\""
-			" height=\"" << height - margins[bottom] - margins[top] << "em\""
-			">\n";
+			// NOTE: plot area starts
+			"<svg"
+			" x=\"" << margins[left] << "%\" y=\"" << margins[top] << "%\""
+			" width=\"" << 100 - margins[right] - margins[left] << "%\""
+			" height=\"" << 100 - margins[bottom] - margins[top] << "%\""
+			// plot coordinates: [0; 1] + a little extra to prevent antialiased lines from being cropped
+			" viewBox=\"-.01 -.01 1.02 1.02\""
+			">"
+		;
 
-		// axes
-		ss << "<line x1=\"0%\" y1=\"100%\" x2=\"0%\" y2=\"0%\"/>"
-			"<line x1=\"0%\" y1=\"100%\" x2=\"100%\" y2=\"100%\"/>";
+		// the plot area is just a series of lines, and there's a very
+		// effective representation for that; who knew?
+		ss << "<path vector-effect=\"non-scaling-stroke\" d=\"\n";
+
+		// TODO: Tufte-style axes (start and end on a "round" value)
+		ss << "M0,1 L0,0 M0,1 L1,1\n";
 
 		for (size_t i = 0; i < lines.size(); i++) {
-			for (size_t j = 1; j < lines[i].n; j++)
-				// screw this, polylines and paths are unsuitable for relative or physical positions
-				// let's do it the hard way, then
-				ss << "<line"
-					" x1=\"" << (lines[i].x[j-1] - axes.x[0])/(axes.x[1] - axes.x[0]) * 100 << "%\""
-					" y1=\"" << (axes.y[1] - lines[i].y[j-1])/(axes.y[1] - axes.y[0]) * 100 << "%\""
-					" x2=\"" << (lines[i].x[j] - axes.x[0])/(axes.x[1] - axes.x[0]) * 100 << "%\""
-					" y2=\"" << (axes.y[1] - lines[i].y[j])/(axes.y[1] - axes.y[0]) * 100 << "%\""
-					"/>";
-			ss << "\n";
+			for (size_t j = 0; j < lines[i].n; j++)
+				ss << (j ? 'L' : 'M')
+					<< (lines[i].x[j] - axes.x[0])/(axes.x[1] - axes.x[0]) << ','
+					<< (axes.y[1] - lines[i].y[j])/(axes.y[1] - axes.y[0]) << ' ';
+			ss << '\n';
 		}
-
-		ss << "</svg>\n"; // plot area ends, can draw labels now
+		ss << "\"/></svg>\n"; // NOTE: plot area ends
 
 		ss << "</g></svg>\n";
 
