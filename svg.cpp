01f9caebc0110897078f80834d6a99eda9ae6b8f
Ivan Krylov <krylov.r00t@gmail.com>
2019-06-15 13:19:03 +0300

    we've got axis labels!
    TODO: code cleanup
    - scale function instead of repeated (x - xmin)/(xmax - xmin)
    - prevent repeated axis labels loop somehow (store them in array?)
    - un-hardcode some more things like label offset, line parameters?
    - make a real class out of this

--- svg.cpp
+++ svg.cpp
@@ -5,14 +5,14 @@
 #include <vector>
 
 static double nicenum(double x) {
-	static const double mult[] = { 2., 5., 10. };
 	// round down to power of 10
 	double rnd = std::pow(10, std::floor(std::log(x)/std::log(10)));
 	// how far is it from the original number?
 	double diff = x - rnd, ret = rnd;
 	// 1, 2, 5, 10 are "nice" multipliers, so try multiplying by them
 	// in case it brings our estimate closer to original
-	for (int i = 0; i < sizeof(mult)/sizeof(*mult); i++)
+	static const double mult[] = { 2., 5., 10. };
+	for (unsigned i = 0; i < sizeof(mult)/sizeof(*mult); i++)
 		if (std::abs(x - mult[i] * rnd) < diff) {
 			diff = std::abs(x - mult[i] * rnd);
 			ret = mult[i] * rnd;
@@ -71,7 +71,10 @@ struct svgplot {
 			" viewBox=\"" << -margins[left] << ' ' << -margins[top] << ' '
 			<< 1 + margins[right] << ' ' << 1 + margins[bottom] << "\""
 			">\n"
-			"<g style=\"fill: none; stroke: black; stroke-width: 1px;\">\n"
+			"<style>\n"
+			"path { fill: none; stroke: black; stroke-width: 1px; }\n"
+			"text { font-size: " << margins[bottom] / 2 << "px; }\n"
+			"</style>\n"
 		;
 
 		// the plot area is just a series of lines, and there's a very
@@ -88,23 +91,41 @@ struct svgplot {
 		};
 		// 2. Make the axes cover the actual data range (not extend to [0,1])
 		ss << "M" << (range.x[0] - axes.x[0])/(axes.x[1] - axes.x[0]) << ",1"
-			" L" << (range.x[1] - axes.x[0])/(axes.x[1] - axes.x[0]) << ",1"
-			" M0," << (axes.y[1] - range.y[0])/(axes.y[1] - axes.y[0])
-			<< " L0," << (axes.y[1] - range.y[1])/(axes.y[1] - axes.y[0])
+			"L" << (range.x[1] - axes.x[0])/(axes.x[1] - axes.x[0]) << ",1"
+			"M0," << (axes.y[1] - range.y[0])/(axes.y[1] - axes.y[0])
+			<< "L0," << (axes.y[1] - range.y[1])/(axes.y[1] - axes.y[0])
+			<< "\n"
 		;
-		// TODO: 3. Make ticks at "nice" positions (multiples of dx, dy)
+		// 3. Make ticks at "nice" positions (multiples of dx, dy)
+		for (double x = dx * ceil(range.x[0] / dx); x <= dx * floor(range.x[1] / dx); x += dx)
+			ss << "M" << (x - axes.x[0])/(axes.x[1] - axes.x[0]) << ",1"
+				"L" << (x - axes.x[0])/(axes.x[1] - axes.x[0]) << ",1.01";
+		for (double y = dy * ceil(range.y[0] / dy); y <= dy * floor(range.y[1] / dy); y += dy)
+			ss << "M0," << (axes.y[1] - y)/(axes.y[1] - axes.y[0])
+				<< "L-.01," << (axes.y[1] - y)/(axes.y[1] - axes.y[0]);
+		ss << "\n";
 
 		// now draw the actual lines
 		for (size_t i = 0; i < lines.size(); i++) {
 			for (size_t j = 0; j < lines[i].n; j++)
 				ss << (j ? 'L' : 'M')
 					<< (lines[i].x[j] - axes.x[0])/(axes.x[1] - axes.x[0]) << ','
-					<< (axes.y[1] - lines[i].y[j])/(axes.y[1] - axes.y[0]) << ' ';
+					<< (axes.y[1] - lines[i].y[j])/(axes.y[1] - axes.y[0]);
 			ss << '\n';
 		}
 		ss << "\"/>\n";
 
-		ss << "</g></svg>\n";
+		// oops, axis labels should be repeated here
+		for (double x = dx * ceil(range.x[0] / dx); x <= dx * floor(range.x[1] / dx); x += dx)
+			ss << "<text x=\"" << (x - axes.x[0])/(axes.x[1] - axes.x[0]) << "\""
+				" y=\"" << 1 + margins[bottom] / 2 << "\">" << x << "</text>";
+		ss << "\n";
+		for (double y = dy * ceil(range.y[0] / dy); y <= dy * floor(range.y[1] / dy); y += dy)
+			ss << "<text x=\"" << - margins[left] << "\""
+				" y=\"" << (axes.y[1] - y)/(axes.y[1] - axes.y[0]) << "\">" << y << "</text>";
+		ss << "\n";
+
+		ss << "</svg>\n";
 
 		return ss.str();
 	}
