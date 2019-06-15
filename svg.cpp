d9a9fdcf5b41bdc38d778bf566f93ece78b6f187
Ivan Krylov <krylov.r00t@gmail.com>
2019-06-15 12:49:19 +0300

    on my way to Tufte-style axes with "nice" ticks
    - position everything in plot coordinates using viewBox
      (plot starts at (-leftmargin, -topmargin), etc)
    - implement "nice numbers" algorithm from [Heckbert, 1990]
    - widen the plot region using the "nice number" just outside
      data range, but start the axes at actual data range
    
    TODO: actual ticks, labels (font size!)

--- svg.cpp
+++ svg.cpp
@@ -1,7 +1,24 @@
 #include <algorithm>
+#include <cmath>
 #include <limits>
 #include <sstream>
 #include <vector>
+
+static double nicenum(double x) {
+	static const double mult[] = { 2., 5., 10. };
+	// round down to power of 10
+	double rnd = std::pow(10, std::floor(std::log(x)/std::log(10)));
+	// how far is it from the original number?
+	double diff = x - rnd, ret = rnd;
+	// 1, 2, 5, 10 are "nice" multipliers, so try multiplying by them
+	// in case it brings our estimate closer to original
+	for (int i = 0; i < sizeof(mult)/sizeof(*mult); i++)
+		if (std::abs(x - mult[i] * rnd) < diff) {
+			diff = std::abs(x - mult[i] * rnd);
+			ret = mult[i] * rnd;
+		}
+	return ret;
+}
 
 struct svgplot {
 	struct data {
@@ -9,35 +26,35 @@ struct svgplot {
 		std::size_t n;
 	};
 
-	unsigned int ticks; // how many ticks to place on the axes, TODO
+	unsigned int ticks; // how many ticks to place on the axes
 
 	struct {
 		double x[2], y[2]; // plot coordinates: {min, max}
-	} axes;
+	} range;
 
 	enum mar { bottom, left, top, right, mar_max };
-	unsigned int margins[mar_max]; // %
+	double margins[mar_max]; // fraction
 
 	std::vector<data> lines;
 
 	svgplot() : ticks(4) {
 		// limits start with "impossible" values that would
 		// lose to real data in std::min() / std::max() comparison
-		axes.x[0] = std::numeric_limits<double>::max();
-		axes.x[1] = std::numeric_limits<double>::min();
-		axes.y[0] = std::numeric_limits<double>::max();
-		axes.y[1] = std::numeric_limits<double>::min();
+		range.x[0] = std::numeric_limits<double>::max();
+		range.x[1] = std::numeric_limits<double>::min();
+		range.y[0] = std::numeric_limits<double>::max();
+		range.y[1] = std::numeric_limits<double>::min();
 
-		margins[bottom] = margins[left] = 10;
-		margins[top] = margins[right] = 5;
+		margins[bottom] = margins[left] = .10;
+		margins[top] = margins[right] = .05;
 	}
 
 	svgplot & add_line(const double * x, const double * y, std::size_t n) {
 		// update ranges
-		axes.x[0] = std::min(axes.x[0], *std::min_element(x, x+n));
-		axes.y[0] = std::min(axes.y[0], *std::min_element(y, y+n));
-		axes.x[1] = std::max(axes.x[1], *std::max_element(x, x+n));
-		axes.y[1] = std::max(axes.y[1], *std::max_element(y, y+n));
+		range.x[0] = std::min(range.x[0], *std::min_element(x, x+n));
+		range.y[0] = std::min(range.y[0], *std::min_element(y, y+n));
+		range.x[1] = std::max(range.x[1], *std::max_element(x, x+n));
+		range.y[1] = std::max(range.y[1], *std::max_element(y, y+n));
 		// store the pointers
 		data l = {x, y, n};
 		lines.push_back(l);
@@ -51,25 +68,33 @@ struct svgplot {
 
 		ss << "<svg"
 			" version=\"1.2\" baseProfile=\"tiny\" xmlns=\"http://www.w3.org/2000/svg\""
+			" viewBox=\"" << -margins[left] << ' ' << -margins[top] << ' '
+			<< 1 + margins[right] << ' ' << 1 + margins[bottom] << "\""
 			">\n"
 			"<g style=\"fill: none; stroke: black; stroke-width: 1px;\">\n"
-			// NOTE: plot area starts
-			"<svg"
-			" x=\"" << margins[left] << "%\" y=\"" << margins[top] << "%\""
-			" width=\"" << 100 - margins[right] - margins[left] << "%\""
-			" height=\"" << 100 - margins[bottom] - margins[top] << "%\""
-			// plot coordinates: [0; 1] + a little extra to prevent antialiased lines from being cropped
-			" viewBox=\"-.01 -.01 1.02 1.02\""
-			">"
 		;
 
 		// the plot area is just a series of lines, and there's a very
 		// effective representation for that; who knew?
 		ss << "<path vector-effect=\"non-scaling-stroke\" d=\"\n";
 
-		// TODO: Tufte-style axes (start and end on a "round" value)
-		ss << "M0,1 L0,0 M0,1 L1,1\n";
+		// Tufte-style axes:
+		// 1. Give the plot some space (start at outward "nice" tick position)
+		double dx = nicenum((range.x[1]-range.x[0])/(ticks - 1)),
+			dy = nicenum((range.y[1]-range.y[0])/(ticks - 1));
+		struct { double x[2], y[2]; } axes = {
+			{ dx * floor(range.x[0] / dx), dx * ceil(range.x[1] / dx) },
+			{ dy * floor(range.y[0] / dy), dy * ceil(range.y[1] / dy) }
+		};
+		// 2. Make the axes cover the actual data range (not extend to [0,1])
+		ss << "M" << (range.x[0] - axes.x[0])/(axes.x[1] - axes.x[0]) << ",1"
+			" L" << (range.x[1] - axes.x[0])/(axes.x[1] - axes.x[0]) << ",1"
+			" M0," << (axes.y[1] - range.y[0])/(axes.y[1] - axes.y[0])
+			<< " L0," << (axes.y[1] - range.y[1])/(axes.y[1] - axes.y[0])
+		;
+		// TODO: 3. Make ticks at "nice" positions (multiples of dx, dy)
 
+		// now draw the actual lines
 		for (size_t i = 0; i < lines.size(); i++) {
 			for (size_t j = 0; j < lines[i].n; j++)
 				ss << (j ? 'L' : 'M')
@@ -77,7 +102,7 @@ struct svgplot {
 					<< (axes.y[1] - lines[i].y[j])/(axes.y[1] - axes.y[0]) << ' ';
 			ss << '\n';
 		}
-		ss << "\"/></svg>\n"; // NOTE: plot area ends
+		ss << "\"/>\n";
 
 		ss << "</g></svg>\n";
 
